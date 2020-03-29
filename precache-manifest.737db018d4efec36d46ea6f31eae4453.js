7113b1ea38091a0e70eeb27ac8ec641c7989e5e7
rahulnag <rahulnag625@gmail.com>
2020-03-29 21:50:58 +0530

    Updates

---
+++ precache-manifest.737db018d4efec36d46ea6f31eae4453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e954f19d5fd1ff833fbf63013f2bb8e",
+    "url": "/covid19live/index.html"
+  },
+  {
+    "revision": "2baf568bba557af4712b",
+    "url": "/covid19live/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "70ef9b0b3afb21a670cf",
+    "url": "/covid19live/static/js/2.0bc73278.chunk.js"
+  },
+  {
+    "revision": "2baf568bba557af4712b",
+    "url": "/covid19live/static/js/main.9ac76d00.chunk.js"
+  },
+  {
+    "revision": "67b2d62bf790157b8032",
+    "url": "/covid19live/static/js/runtime~main.ec8e2ca4.js"
+  }
+]);
